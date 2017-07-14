3678b49b77f9ed229bde0413873b2d3bd05862e7
Denis Dulovic <denis.dulovic@gmail.com>
2017-07-14 15:49:09 +0200

    first

--- youtube audio download/MainWindow.xaml.cs
+++ youtube audio download/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,86 @@ namespace youtube_audio_download
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string link = "";
+        private string arg1 = "";
+        private string arg2 = "";
+
+        private readonly string[] argsList1 = {"bestaudio", "bestvideo"};
+        private readonly string[] argsList2 = {"none", "best", "mp3", "aac"};
+
         public MainWindow()
         {
             InitializeComponent();
+
+            formatSelect.ItemsSource = argsList1;
+            formatSelect.SelectedIndex = 0;
+            outputSelect.ItemsSource = argsList2;
+            outputSelect.SelectedIndex = 0;
+            arg1 = "-f " + formatSelect.SelectedItem;
+            //arg2 = outputSelect.SelectedItem.ToString();
+            outputTBox.IsReadOnly = true;
         }
+
+        private void download(string arg1, string arg2)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = Environment.CurrentDirectory + "\\thingythings\\youtube-dl.exe";
+            process.StartInfo.Arguments = arg1 + arg2 + " " + link;
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.OutputDataReceived += process_OutputDataReceived;
+            process.EnableRaisingEvents = true;
+                
+            process.Start();
+
+            process.BeginOutputReadLine();
+
+            //process.WaitForExit();
+            //process.Close();
+
+            process.Exited += process_HasExited;
+        }
+
+        private void process_HasExited(object sender, EventArgs e)
+        {
+            MessageBox.Show("DONE!", "Done", MessageBoxButton.OK);
+        }
+
+        private void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Dispatcher.Invoke((Action) (() =>
+            {
+                outputTBox.AppendText(e.Data + "\n");
+                outputTBox.ScrollToEnd();
+            }));
+            
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            download(arg1, arg2);
+        }
+
+        private void linkTBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            link = linkTBox.Text;
+        }
+
+
+        private void formatSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            arg1 = "-f " + formatSelect.SelectedItem;
+        }
+
+        private void outputSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (outputSelect.SelectedIndex != 0)
+                arg2 = " -x --audio-format " + outputSelect.SelectedItem;
+        }
+
     }
 }
